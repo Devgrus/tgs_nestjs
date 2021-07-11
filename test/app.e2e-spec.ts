5fcb8aa88116ee2d74ef146f94a718efbaf086fe
Chanwoo KIM <chanwoo.kim@outlook.fr>
2021-07-11 22:24:55 +0200

    feat(test): delete unused file

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,11 +1,9 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+// import { AppModule } from './../src/app.module';
 import { AppController } from './../src/app.controller';
 import { AppService } from './../src/app.service';
-import { HistoryController } from './../src/history/history.controller';
-import { HistoryService } from './../src/history/history.service';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
@@ -27,26 +25,4 @@ describe('AppController (e2e)', () => {
       .expect(200)
       .expect('Hello World!');
   });
-  it('/ (GETODAS2)', () => {
-    return request(app.getHttpServer())
-      .get('/history?purchaseId=1')
-      .expect(200);
-  });
-
-  // const offerRequestData = {
-  //   fromLatitude: 48.870377,
-  //   fromLongitude: 2.370615,
-  //   nbPassengers: 1,
-  //   toLatitude: 48.882719,
-  //   toLongitude: 2.322451,
-  // };
-  // it('POST', () => {
-  //   return request(app.getHttpServer())
-  //     .post('/offer')
-  //     .send(JSON.stringify(offerRequestData))
-  //     .expect(201);
-  // });
-  // describe('/offer', () => {
-
-  // });
 });
